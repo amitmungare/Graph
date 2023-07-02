4deb0ae9f9b804596198f8939413472f04ea6486
amitmungare <amungare27@gmail.com>
2023-07-03 01:04:42 +0530

    BFS

---
+++ src/Edge.java
@@ -0,0 +1,17 @@
+public class Edge {
+
+    int src;
+    int dest;
+    int weight;
+
+    public Edge(int s, int d){
+        this.src=s;
+        this.dest=d;
+    }
+
+    public Edge(int s, int d, int w){
+        this.src=s;
+        this.dest=d;
+        this.weight=w;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,43 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
+public class Main {
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for (int i=0; i< graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,2,2));
+
+        graph[1].add(new Edge(1,2,10));
+        graph[1].add(new Edge(1,3,0));
+
+        graph[2].add(new Edge(2,0,2));
+        graph[2].add(new Edge(2,1,10));
+        graph[2].add(new Edge(2,3,-1));
+
+        graph[3].add(new Edge(3,1,0));
+        graph[3].add(new Edge(3,2,-1));
+    }
+
+    public static void printGraph(ArrayList<Edge> graph[]){
+        for (int g=0; g< graph.length; g++){
+            System.out.print(g+" : ");
+            for(int i=0; i<graph[g].size(); i++){
+                Edge e = graph[g].get(i);
+                System.out.print(e.dest+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int v=4;
+        ArrayList<Edge> graph[] = new ArrayList[v];
+
+        createGraph(graph);
+
+        printGraph(graph);
 
     }
 }
